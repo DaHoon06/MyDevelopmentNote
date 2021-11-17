881903f60e1e8c28cf4ba50e623d12f5afecde1b
dahoon06 <dahoon226@naver.com>
2021-11-17 17:09:58 +0900

    #17 ejs chartJS 활용 백업
    프로젝트 중 일부

---
+++ project/634/b-dash-board.js
@@ -0,0 +1,224 @@
+module.exports = async({ req, res, next, mongo, projectId }) => {
+    try {
+
+        console.log('--BUSAN--')
+        const LOCAL_NUM = '2'; // 지역 2= 부산
+        const SNUM = +projectId;
+
+        const sendData = {
+            quotaAgeData:{
+                age : [0,0,0,0],
+                successData : [0,0,0,0],
+            },
+            quotaGenderData:{
+                male:'0',
+                female:'0'
+            },
+            qDataQ1_1: [0,0,0,0,0,0,0],
+            qDataQ1_2: [0,0,0,0,0,0,0],
+            qDataQ2 : [0,0,0,0,0,0,0,0],
+            qDataQ3 : [0,0,0,0,0,0,0,0,0,0,0],
+        }
+
+        const questions = await mongo.db('getData').collection('QUESTION').find({_id:+projectId}).toArray();
+
+
+        const ageQuotaData = await getAgeQuota(mongo,SNUM,LOCAL_NUM);
+        await setQAgeQuotaData(ageQuotaData,sendData);
+
+        // TOTAL QUOTA
+        const genderQuotaData = await getGenderQuota(mongo,SNUM,LOCAL_NUM);
+        await setGenderQuota(genderQuotaData,sendData);
+
+        const q1_1Data = await getQ1_1Data(mongo,SNUM,LOCAL_NUM);
+        await setQ1_1Data(q1_1Data,sendData);
+
+        const q1_2Data = await getQ1_2Data(mongo,SNUM,LOCAL_NUM);
+        await setQ1_2Data(q1_2Data,sendData);
+
+
+        // Q2 DATA
+        const [q2Data] = await getQ2Data(mongo,SNUM,LOCAL_NUM);
+        await setQ2Data(q2Data,sendData);
+
+        // Q3 DATA
+        const [q3Data] = await getQ3Data(mongo,SNUM,LOCAL_NUM);
+        await setQ3Data(q3Data,sendData);
+
+        // AGE QUOTA
+
+
+
+        if(questions === null || questions === undefined) throw new Error('존재하지 않는 설문입니다.');
+        // const { TITLE: surveyTitle} = questions;
+
+        return {
+            projectId,
+            surveyTitle:'',
+            result: true,
+            ...sendData
+        };
+
+    } catch (e) {
+
+        return {
+            error: e
+        }
+
+    }
+}
+
+//COMPLETE
+async function getGenderQuota(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {$group: {
+                _id: '$SQ2',
+                cnt: {$sum: 1}
+            }},
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function getAgeQuota(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {$group: {
+                _id: '$SQ3_R',
+                cnt: {$sum: 1}
+            }},
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function getQ1_1Data(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {$group: {
+                _id: '$Q1_1',
+                sum: {$sum: 1}
+            }
+        },
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function getQ1_2Data(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {$group: {
+                _id: '$Q1_2',
+                sum: {$sum: 1}
+            }
+        },
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function getQ2Data(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {
+            $group : {
+                _id : 'ALL' ,
+                Q2_1 : { $avg : {$toInt : "$Q2_1"}},
+                Q2_2 : { $avg : {$toInt : "$Q2_2"}},
+                Q2_3 : { $avg : {$toInt : "$Q2_3"}},
+                Q2_4 : { $avg : {$toInt : "$Q2_4"}},
+                Q2_5 : { $avg : {$toInt : "$Q2_5"}},
+                Q2_6 : { $avg : {$toInt : "$Q2_6"}},
+                Q2_7 : { $avg : {$toInt : "$Q2_7"}},
+                Q2_8 : { $avg : {$toInt : "$Q2_8"}},
+            }
+        },
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function getQ3Data(mongo,SNUM,LOCAL_NUM){
+    return await mongo.db('getData').collection('DATA_634').aggregate([
+        {$match : { SQ1 : LOCAL_NUM }},
+        {
+            $group : {
+                _id : 'ALL' ,
+                Q3_1 : { $avg : {$toInt : "$Q3_1"}},
+                Q3_2 : { $avg : {$toInt : "$Q3_2"}},
+                Q3_3 : { $avg : {$toInt : "$Q3_3"}},
+                Q3_4 : { $avg : {$toInt : "$Q3_4"}},
+                Q3_5 : { $avg : {$toInt : "$Q3_5"}},
+                Q3_6 : { $avg : {$toInt : "$Q3_6"}},
+                Q3_7 : { $avg : {$toInt : "$Q3_7"}},
+                Q3_8 : { $avg : {$toInt : "$Q3_8"}},
+                Q3_9 : { $avg : {$toInt : "$Q3_9"}},
+                Q3_10 : { $avg : {$toInt : "$Q3_10"}},
+                Q3_11: { $avg : {$toInt : "$Q3_11"}},
+            }
+        },
+        {$sort:{_id:1}}
+    ]).toArray()
+}
+
+//COMPLETE
+async function setQAgeQuotaData(result,sendData){
+
+    for(let i=0; i<=3; i++){
+        if(result[i]?._id) {
+            const ageIndex = +result[i]?._id-2;
+            sendData.quotaAgeData.age[ageIndex] = result[i]?.cnt
+            sendData.quotaAgeData.successData[ageIndex] = (result[i]?.cnt/99).toFixed(3)*100 +'%'
+        }
+    }
+
+
+}
+
+//COMPLETE
+async function setGenderQuota(result,sendData){
+    const [sumMale,sumFemale] = result;
+    if(sumMale && !sumFemale){
+        if(sumMale._id === '1'){
+            sendData.quotaGenderData.male = sumMale.cnt;
+        }else{
+            sendData.quotaGenderData.female = sumFemale.cnt;
+        }
+    }else{
+        sendData.quotaGenderData.male = sumMale.cnt;
+        sendData.quotaGenderData.female = sumFemale.cnt;
+    }
+}
+
+//COMPLETE
+async function setQ2Data(qData,sendData){
+    for(let i=1; i<=8; i++){
+        sendData.qDataQ2[i-1] = qData['Q2_'+i].toFixed(3);
+    }
+}
+
+//COMPLETE
+async function setQ3Data(qData,sendData){
+    for(let i=1; i<=11; i++){
+        sendData.qDataQ3[i-1] = qData['Q3_'+i].toFixed(3);
+    }
+}
+
+//COMPLETE
+async function setQ1_1Data(qData,sendData){
+
+    for(let i=0; i<=qData.length-1; i++){
+        const q1Index = qData[i] && +qData[i]._id;
+        sendData.qDataQ1_1[q1Index-1] = qData[i] && +qData[i].sum;
+    }
+}
+
+//COMPLETE
+async function setQ1_2Data(qData,sendData){
+    for(let i=0; i<=qData.length-1; i++){
+        const q1Index = qData[i] && +qData[i]._id;
+        sendData.qDataQ1_2[q1Index-1] = qData[i] && +qData[i].sum;
+    }
+}
---
+++ project/634/busan.quota.js
@@ -0,0 +1,138 @@
+
+module.exports = async({ req, res, next, mongo, projectId }) => {
+    try {
+        const SNUM = '634'; // 652로 수정해야함
+        const LocalNum = '2';
+        //문항 1 2 3 초기 값 설정
+        const Q1 = {
+            Q_1: [0,0,0,0,0,0,0],
+            Q_2: [0,0,0,0,0,0,0],
+
+        }
+        //남여비율
+        const genderData = await SQ2(mongo,LocalNum);
+        const cnt = genderData['0'].cnt + genderData['1'].cnt;
+        //연령별
+        const generationRatio = await SQ3(mongo,LocalNum);
+        //응답 평균
+        const total = await totalSuccess(cnt,generationRatio);
+
+        const q1_1 = await Q1_1(mongo,LocalNum,Q1);
+        const q1_2 = await Q1_2(mongo,LocalNum,Q1);
+
+        const test = await dataTest(mongo,LocalNum);
+
+        return {
+            gender: [
+                genderData['0'].cnt,
+                genderData['1'].cnt],
+            age: [
+                generationRatio['0'].cnt,
+                generationRatio['1'].cnt,
+                generationRatio['2'].cnt,
+                generationRatio['3'].cnt,
+            ],
+            total: total,
+            q1: q1_1,
+            q2: q1_2,
+        };
+    } catch (e) {
+        return {
+            error: e
+        }
+    }
+
+    //남여 비율
+    async function SQ2 (mongo,LocalNum) {
+        return  await mongo.db('getData').collection('DATA_634').aggregate([
+            {$match: {SQ1: LocalNum}}, //일본에 맞춰서 변경
+            {$group:{
+               _id : "$SQ2", //해당 그룹에 맟춰서 남 여 총합을 구함
+               cnt : { $sum:1 }
+            }},
+            {$sort: {'_id': 1}},
+        ]).toArray();
+
+    }
+    //연령
+    async function SQ3(mongo,LocalNum){
+        return await mongo.db('getData').collection('DATA_634').aggregate([
+            {$match: {SQ1: LocalNum}},
+            {$group: {
+                _id: '$SQ3_R',
+                cnt: {$sum: 1}
+            }},
+            {$sort: {'_id': 1}}
+        ]).toArray();
+    }
+
+    async function totalSuccess(cnt,generationRatio){
+        //배열을 만들어서 넘길까
+       let result1 = Math.round(generationRatio['0'].cnt / 99 * 100);
+       let result2 = Math.round(generationRatio['1'].cnt / 99 * 100);
+       let result3 = Math.round(generationRatio['2'].cnt / 99 * 100);
+       let result4 = Math.round(generationRatio['3'].cnt / 99 * 100);
+
+        return {
+            result1,
+            result2,
+            result3,
+            result4
+        };
+    }
+
+    async function Q1_1(mongo,LocalNum,Q1){
+        let data1 = [];
+        const q1_1 = await mongo.db('getData').collection('DATA_634').aggregate([
+            {$match: {SQ1: LocalNum}},
+            {$group: {
+                    _id: '$Q1_1',
+                    cnt: {$sum: 1}
+                }},
+            {$sort: {'_id': 1}}
+        ]).toArray();
+
+        for(let i=0;i<q1_1.length;i++){
+            if(!q1_1[i]){
+                data1.push(0);
+            }
+            data1.push(q1_1[i].cnt);
+        }
+        return data1;
+    }
+
+    async function Q1_2(mongo,LocalNum,Q1){
+        let index = 1;
+        const q1_2 = await mongo.db('getData').collection('DATA_634').aggregate([
+            {$match: {SQ1: LocalNum}},
+            {$group: {
+                    _id: '$Q1_2',
+                    cnt: {$sum: 1}
+                }},
+            {$sort: {'_id': 1}}
+        ]).toArray();
+        console.log(q1_2[0]._id);
+
+        for(let i=0;i<q1_2.length;i++){
+            //1부터 시작
+            if(q1_2[i]._id !== index){
+                Q1.Q_2[i] = 0;
+                index++;
+            }
+            Q1.Q_2[i] = q1_2[i].cnt;
+        }
+        console.log(Q1.Q_2);
+        return Q1.Q_2;
+    }
+
+    async function dataTest(mongo){
+        return await mongo.db('getData').collection('DATA_634').aggregate([
+        ]).toArray();
+    }
+
+
+
+
+}
+
+

ef1a358adad1c145d4471da8acb933516f56bd68
dahoon06 <dahoon226@naver.com>
2021-12-20 21:02:36 +0900

    #32 REACT Sass 학습

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -4,6 +4,7 @@ import Info from "./components/Hooks/useState/Info";
 import Counter from "./components/Hooks/useReducer/Counter";
 import InfoReducer from "./components/Hooks/useReducer/InfoReducer";
 import Average from "./components/Hooks/useMemo/Average";
+import SassComponent from "./components/CssSample/SassComponent";
 
 const App = () => {
     const [visible, setVisible] = useState(false);
@@ -17,6 +18,8 @@ const App = () => {
             <InfoReducer />
             <br/><br/><br/>
             <Average />
+            <br/><br/><br/>
+            <SassComponent />
         </div>
     );
 }

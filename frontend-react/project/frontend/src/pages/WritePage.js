59e46bbc44d3bada9596f9eace9e48563d7dcaaf
dahoon06 <dahoon226@naver.com>
2022-01-12 22:03:03 +0900

    #45 React 글작성, 태그작성

--- frontend-react/project/frontend/src/pages/WritePage.js
+++ frontend-react/project/frontend/src/pages/WritePage.js
@@ -1,10 +1,14 @@
-import Editor from "../components/write/Editor";
 import Responsive from "../components/common/Responsive";
+import EditorContainers from "../containers/write/EditorContainers";
+import TagBoxContainer from "../containers/write/TagBoxContainer";
+import WriteActionButtonContainer from "../containers/write/WriteActionButtonContainer";
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
+            <EditorContainers />
+            <TagBoxContainer />
+            <WriteActionButtonContainer />
         </Responsive>
     )
 };

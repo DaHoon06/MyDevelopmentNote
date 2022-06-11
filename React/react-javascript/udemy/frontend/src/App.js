0f6746d2e337b5583c24a24fab2b8cb7c924e51a
dahoon06 <dahoon226@naver.com>
2022-06-11 16:28:46 +0900

    udemy: 동적 렌더링

--- React/react-javascript/udemy/frontend/src/App.js
+++ React/react-javascript/udemy/frontend/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Expense from "./components/Expenses/Expense";
 import NewExpense from "./components/NewExpenses/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSE = [
+    {id: 'e1', title: 'Hello', amount: '500원', date: new Date(2022, 6, 8)},
+    {id: 'e2', title: 'world', amount: '1,500원', date: new Date(2022, 6, 9)},
+    {id: 'e3', title: 'react', amount: '2,500원', date: new Date(2022, 6, 10)},
+    {id: 'e4', title: 'vue', amount: '1,500원', date: new Date(2022, 6, 10)},
+]
 
 const App = () => {
-    const expenses = [
-        {id: 'e1', title: 'Hello', amount: '500원', date: new Date(2022, 6, 8)},
-        {id: 'e2', title: 'world', amount: '1,500원', date: new Date(2022, 6, 9)},
-        {id: 'e3', title: 'react', amount: '2,500원', date: new Date(2022, 6, 10)},
-        {id: 'e4', title: 'vue', amount: '1,500원', date: new Date(2022, 6, 10)},
-    ]
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
 
     const addExpenseHandler = expense => {
-        console.log(expense);
-        
+        setExpenses((prevExpense) => {
+            return [expense, ...prevExpense];
+        });
     }
     return (
         <div className='expenses'>

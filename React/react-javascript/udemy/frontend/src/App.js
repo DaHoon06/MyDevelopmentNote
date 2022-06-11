d59c9c8c1433e7e08265ca8848c9e20fd144d5a9
dahoon06 <dahoon226@naver.com>
2022-06-11 16:13:55 +0900

    udemy: useState 개념

--- React/react-javascript/udemy/frontend/src/App.js
+++ React/react-javascript/udemy/frontend/src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import Expense from "./components/Expense";
+import Expense from "./components/Expenses/Expense";
+import NewExpense from "./components/NewExpenses/NewExpense";
 
-function App() {
+const App = () => {
     const expenses = [
         {id: 'e1', title: 'Hello', amount: '500원', date: new Date(2022, 6, 8)},
         {id: 'e2', title: 'world', amount: '1,500원', date: new Date(2022, 6, 9)},
         {id: 'e3', title: 'react', amount: '2,500원', date: new Date(2022, 6, 10)},
         {id: 'e4', title: 'vue', amount: '1,500원', date: new Date(2022, 6, 10)},
     ]
+
+    const addExpenseHandler = expense => {
+        console.log(expense);
+        
+    }
     return (
-        <div>
-            <div className='expenses'>
-                <Expense items={expenses} />
-            </div>
+        <div className='expenses'>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expense items={expenses} />
         </div>
+
     );
 }
 
---
+++ React/react-javascript/udemy/frontend/src/components/Expenses/Expense.js
@@ -0,0 +1,42 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import './Expense.css'
+import { useState } from "react";
+
+const Expense = (data) => {
+    const [filterYear, setFilterYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        console.log(selectedYear);
+        setFilterYear(selectedYear);
+    }
+    return (
+        <div>
+            <ExpenseFilter selected={filterYear} onChageFilter={filterChangeHandler} />
+            <Card className='expenses'>
+                <ExpenseItem
+                    title={data.items[0].title}
+                    amount={data.items[0].amount}
+                    date={data.items[0].date}
+                />
+                <ExpenseItem
+                    title={data.items[1].title}
+                    amount={data.items[1].amount}
+                    date={data.items[1].date}
+                />
+                <ExpenseItem
+                    title={data.items[2].title}
+                    amount={data.items[2].amount}
+                    date={data.items[2].date}
+                />
+                <ExpenseItem
+                    title={data.items[3].title}
+                    amount={data.items[3].amount}
+                    date={data.items[3].date}
+                />
+            </Card>
+        </div>
+    );
+}
+export default Expense
